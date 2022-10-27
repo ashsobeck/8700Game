e95f3127809732ff3fd9c6b047849a16c85a88f8
Will Sherrer <wsherre@clemson.edu>
2022-10-27 09:58:49 -0400

    Add snake head and direction

--- snake.py
+++ snake.py
@@ -4,7 +4,10 @@ import pygame
 class Snake(pygame.sprite.Sprite):
     def __init__(self, screen, width, height, block_size):
         super().__init__()
-        self.image = pygame.image.load("icons/pumpkin.png")
+        self.snake_up = pygame.image.load("icons/Snake_Head_Up.png")
+        self.snake_down = pygame.image.load("icons/Snake_Head_Down.png")
+        self.snake_right = pygame.image.load("icons/Snake_Head_Right.png")
+        self.snake_left = pygame.image.load("icons/Snake_Head_Left.png")
         self.screen = screen
         self.x = 300
         self.y = 300
@@ -19,6 +22,7 @@ class Snake(pygame.sprite.Sprite):
         self.x += self.x_change
         self.y += self.y_change
 
+        # stop snek from going out of bounds
         if self.x <= 0:
             self.x = 0
         elif self.x >= self.width - self.block_size:
@@ -27,18 +31,31 @@ class Snake(pygame.sprite.Sprite):
             self.y = 0
         elif self.y >= self.height - self.block_size:
             self.y = self.height - self.block_size
-        self.screen.blit(self.image, (self.x, self.y))
+
+        # draw snek orientation based off of head location
+        if self.direction == 'left':
+            self.screen.blit(self.snake_left, (self.x, self.y))
+        elif self.direction == 'right':
+            self.screen.blit(self.snake_right, (self.x, self.y))
+        elif self.direction == 'down':
+            self.screen.blit(self.snake_down, (self.x, self.y))
+        else:
+            self.screen.blit(self.snake_up, (self.x, self.y))
 
     def change_direction(self, direction):
         if direction == 'left':
+            self.direction = 'left'
             self.x_change = -3
             self.y_change = 0
         elif direction == 'right':
+            self.direction = 'right'
             self.x_change = 3
             self.y_change = 0
         elif direction == 'up':
+            self.direction = 'up'
             self.x_change = 0
             self.y_change = -3
         else:
+            self.direction = 'down'
             self.x_change = 0
             self.y_change = 3
