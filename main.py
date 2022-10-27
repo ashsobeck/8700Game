9d5ca5e40b7451014c20f726ccffd6b8f7b9e992
Will Sherrer <wsherre@clemson.edu>
2022-10-27 18:27:21 -0400

    Snek can move now

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from snake import Snake
 # Init pygame
 pygame.init()
 
+# 32 pixel images. 30 Cells in x direction, 20 cells in Y direction
 WIDTH = 960
 HEIGHT = 640
 BLOCK_SIZE = 32
@@ -21,26 +22,32 @@ pygame.display.set_caption("Haunted Ghost Snake")
 icon = pygame.image.load("icons/scream.png")
 pygame.display.set_icon(icon)
 
-
-s = Snake(screen, WIDTH, HEIGHT, BLOCK_SIZE)
+snake = Snake(screen, WIDTH, HEIGHT)
 running = True
+
+SCREEN_UPDATE = pygame.USEREVENT
+# set a timer to go off every 200 milliseconds
+pygame.time.set_timer(SCREEN_UPDATE, 200)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        if event.type == SCREEN_UPDATE:
+            # when the timer goes off then move the snake
+            snake.move_snake()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT or event.key == pygame.K_a:
-                s.change_direction('left')
-            if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
-                s.change_direction('right')
-            if event.key == pygame.K_UP or event.key == pygame.K_w:
-                s.change_direction('up')
-            if event.key == pygame.K_DOWN or event.key == pygame.K_s:
-                s.change_direction('down')
+            if (event.key == pygame.K_LEFT or event.key == pygame.K_a) and snake.direction != 'right':
+                snake.direction = 'left'
+            if (event.key == pygame.K_RIGHT or event.key == pygame.K_d) and snake.direction != 'left':
+                snake.direction = 'right'
+            if (event.key == pygame.K_UP or event.key == pygame.K_w) and snake.direction != 'down':
+                snake.direction = 'up'
+            if (event.key == pygame.K_DOWN or event.key == pygame.K_s) and snake.direction != 'up':
+                snake.direction = 'down'
 
     screen.blit(background_image, (0, 0))
-    s.draw()
+    snake.draw_snake()
+    pygame.display.update()
     # this means the game operates at 60 fps, just makes the movements smoother and not choppy
     clock.tick(60)
-    pygame.display.update()
--- snake.py
+++ snake.py
@@ -1,61 +1,117 @@
 import pygame
 
+BLOCK_SIZE = 32
 
-class Snake(pygame.sprite.Sprite):
-    def __init__(self, screen, width, height, block_size):
-        super().__init__()
-        self.snake_up = pygame.image.load("icons/Snake_Head_Up.png")
-        self.snake_down = pygame.image.load("icons/Snake_Head_Down.png")
-        self.snake_right = pygame.image.load("icons/Snake_Head_Right.png")
-        self.snake_left = pygame.image.load("icons/Snake_Head_Left.png")
+
+class Snake():
+    def __init__(self, screen, width, height):
+
         self.screen = screen
-        self.x = 300
-        self.y = 300
+        # keep track of all the snake parts
+        self.head_up = pygame.image.load("icons/Snake_Head_Up.png")
+        self.head_down = pygame.image.load("icons/Snake_Head_Down.png")
+        self.head_right = pygame.image.load("icons/Snake_Head_Right.png")
+        self.head_left = pygame.image.load("icons/Snake_Head_Left.png")
+
+        self.body_hori = pygame.image.load("icons/Snake_Body_Hori.png")
+        self.body_vert = pygame.image.load("icons/Snake_Body_Vert.png")
+
+        self.tail_right = pygame.image.load("icons/Snake_Tail_R.png")
+        self.tail_left = pygame.image.load("icons/Snake_Tail_L.png")
+        self.tail_down = pygame.image.load("icons/Snake_Tail_D.png")
+        self.tail_up = pygame.image.load("icons/Snake_Tail_U.png")
+
+        self.body_tr = pygame.image.load("icons/Snake_Corner_TR.png")
+        self.body_tl = pygame.image.load("icons/Snake_Corner_TL.png")
+        self.body_br = pygame.image.load("icons/Snake_Corner_BR.png")
+        self.body_bl = pygame.image.load("icons/Snake_Corner_BL.png")
+
+        self.direction = 'right'
+        # the body keeps track of the positon of each body part
+        # for simplicity we will keep track of what cell its in
+        self.body = [[15, 10], [14, 10], [13, 10], [12, 10]]
         self.width = width
         self.height = height
-        self.block_size = block_size
-        self.x_change = block_size
-        self.y_change = 0
-        self.direction = 'right'
 
-    def draw(self):
-        self.x += self.x_change
-        self.y += self.y_change
+    def draw_snake(self):
+        self.update_head()
+        self.update_tail()
 
-        # stop snek from going out of bounds
-        if self.x <= 0:
-            self.x = 0
-        elif self.x >= self.width - self.block_size:
-            self.x = self.width - self.block_size
-        if self.y <= 0:
-            self.y = 0
-        elif self.y >= self.height - self.block_size:
-            self.y = self.height - self.block_size
+        for index, block in enumerate(self.body):
+            x = block[0] * BLOCK_SIZE
+            y = block[1] * BLOCK_SIZE
+            # create rectangle around block object. This is for collisions
+            rect = pygame.Rect(x, y, BLOCK_SIZE, BLOCK_SIZE)
 
-        # draw snek orientation based off of head location
+            if index == 0:
+                self.screen.blit(self.head, rect)
+            elif index == self.body.__len__() - 1:
+                self.screen.blit(self.tail, rect)
+            else:
+                prev_block = self.subtract_positions(
+                    self.body[index + 1], block)
+                next_block = self.subtract_positions(
+                    self.body[index - 1], block)
+
+                # body positions
+                if prev_block[0] == next_block[0]:
+                    self.screen.blit(self.body_vert, rect)
+                elif prev_block[1] == next_block[1]:
+                    self.screen.blit(self.body_hori, rect)
+                else:
+                    # corners
+                    if prev_block[0] == -1 and next_block[1] == -1 or prev_block[1] == -1 and next_block[0] == -1:
+                        self.screen.blit(self.body_tl, rect)
+                    elif prev_block[0] == -1 and next_block[1] == 1 or prev_block[1] == 1 and next_block[0] == -1:
+                        self.screen.blit(self.body_bl, rect)
+                    elif prev_block[0] == 1 and next_block[1] == -1 or prev_block[1] == -1 and next_block[0] == 1:
+                        self.screen.blit(self.body_tr, rect)
+                    elif prev_block[0] == 1 and next_block[1] == 1 or prev_block[1] == 1 and next_block[0] == 1:
+                        self.screen.blit(self.body_br, rect)
+
+    def update_head(self):
+        if self.direction == 'right':
+            self.head = self.head_right
+        elif self.direction == 'left':
+            self.head = self.head_left
+        elif self.direction == 'up':
+            self.head = self.head_up
+        else:
+            self.head = self.head_down
+
+    def update_tail(self):
+        # take the cell values of the block beside the tail minues the tail
+        tail = self.subtract_positions(self.body[-2], self.body[-1])
+        if tail[0] == 1:
+            self.tail = self.tail_right
+        elif tail[0] == -1:
+            self.tail = self.tail_left
+        elif tail[1] == -1:
+            self.tail = self.tail_up
+        else:
+            self.tail = self.tail_down
+
+    def subtract_positions(self, first_list: list, second_list: list) -> list:
+        final_position = []
+        # zip allows us to iterate over bost lists at the same time
+        for a, b in zip(first_list, second_list):
+            final_position.append(a - b)
+        return final_position
+
+    def move_snake(self):
+        # get every element except the last one since it dissappears
+        copy = self.body[:-1]
+        front = self.body[0]
         if self.direction == 'left':
-            self.screen.blit(self.snake_left, (self.x, self.y))
+            # move head one block to left in x direction
+            copy.insert(0, [front[0] - 1, front[1]])
         elif self.direction == 'right':
-            self.screen.blit(self.snake_right, (self.x, self.y))
+            # move head one block to right in x direction
+            copy.insert(0, [front[0] + 1, front[1]])
         elif self.direction == 'down':
-            self.screen.blit(self.snake_down, (self.x, self.y))
+            # move head one block down in y direction
+            copy.insert(0, [front[0], front[1] + 1])
         else:
-            self.screen.blit(self.snake_up, (self.x, self.y))
-
-    def change_direction(self, direction):
-        if direction == 'left':
-            self.direction = 'left'
-            self.x_change = -3
-            self.y_change = 0
-        elif direction == 'right':
-            self.direction = 'right'
-            self.x_change = 3
-            self.y_change = 0
-        elif direction == 'up':
-            self.direction = 'up'
-            self.x_change = 0
-            self.y_change = -3
-        else:
-            self.direction = 'down'
-            self.x_change = 0
-            self.y_change = 3
+            # move head one block down in y direction
+            copy.insert(0, [front[0], front[1] - 1])
+        self.body = copy
